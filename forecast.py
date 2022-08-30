4119af7670158c6d0d1831ca8eb1c85d0e725f11
SzymonFit <szymek.fit@gmail.com>
2022-08-30 21:07:17 +0200

    Cz: 1

--- forecast.py
+++ forecast.py
@@ -1,1 +1,21 @@
-print("hello world")
+import requests
+from tkinter import *
+
+def search():
+    pass
+
+canvas = Tk()
+canvas.geometry("500x400")
+canvas.title("Weather")
+
+city = StringVar()
+city_search = Entry(canvas, textvariable=city)
+city_search.pack()
+
+search_button = Button(canvas, text= "Search", width=10, command=search)
+search_button.pack()
+
+
+
+canvas.mainloop()
+
