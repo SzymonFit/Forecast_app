f90ab78c8c52262982950fb8c9707a4917c8cfc3
SzymonFit <szymek.fit@gmail.com>
2022-09-02 20:58:32 +0200

    Final

--- forecast.py
+++ forecast.py
@@ -1,50 +1,78 @@
 import requests
 from tkinter import *
-from configparser import ConfigParser
-
 
 url='https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
 api_key='203d4cb81da56f71571200dbd79006c6'
-
-def get_weather(city):
-    outcome = requests.get (url.format(city,api_key))
-    if outcome:
-        json = outcome.json()
-        city = json['name']
-        country = json['sys']['country']
-        conditions = json['weather'][0]['main']
-        kelvin_temperature = json['main']['temp']
-        celsius_temprerature = kelvin_temperature - 273
-        
-        
-
-
-
-def search():
-    city = city.text.get()
-    weather = get.weather(city)
 
 canvas = Tk()
 canvas.geometry("500x400")
 canvas.title("Weather")
 
 city = StringVar()
-city_search = Entry(canvas, textvariable=city)
-city_search.pack()
+
+enter_city_label = Label(canvas, text="Enter city name", font='bold')
+enter_city_label.pack()
+
+search_city = Entry(canvas, textvariable=city)
+search_city.pack()
+
 search_button = Button(canvas, text= "Search", width=10, command=search)
 search_button.pack()
-location = Label(canvas, text='Location: ', font="bold")
-location.pack()
-weather_conditions=Label(canvas, text="Weather conditions: ", font='bold')
-temperature = Label(canvas, text="Temperature: ", font="bold")
-temperature.pack()
-felt_temperature = Label(canvas, text="Felt temperature: ", font='bold')
-felt_temperature.pack()
-pressure = Label(canvas, text="Pressure: ", font="bold")
-pressure.pack()
-humidity = Label(canvas, text='Humidity: ', font='bold')
-humidity.pack()
-wind_speed = Label(canvas, text='Wind speed: ', font='bold')
-wind_speed.pack()
+
+city_label = Label(canvas, text="City: ", font="bold")
+city_label.pack()
+
+country_label = Label(canvas, text='Country: ', font="bold")
+country_label.pack()
+
+weather_conditions_label=Label(canvas, text="Weather conditions: ", font='bold')
+weather_conditions_label.pack()
+
+temperature_label = Label(canvas, text="Temperature:  (°C)", font="bold")
+temperature_label.pack()
+
+felt_temperature_label = Label(canvas, text="Felt temperature: (°C)", font='bold')
+felt_temperature_label.pack()
+
+pressure_label = Label(canvas, text="Pressure: (hPa)", font="bold")
+pressure_label.pack()
+
+humidity_label = Label(canvas, text='Humidity: (%)', font='bold')
+humidity_label.pack()
+
+wind_speed_label = Label(canvas, text='Wind speed: (m/s)', font='bold')
+wind_speed_label.pack()
 
 canvas.mainloop()
+
+def get_weather(city):
+    data_get_weather = requests.get(url.format(city,api_key))
+    if data_get_weather:
+        data_to_json = data_get_weather.json()
+        city = data_to_json['name']
+        country = data_to_json['sys']['country']
+        conditions = data_to_json['weather'][0]['main']
+        celsius_temperature = int(data_to_json['main']['temp'] - 273)
+        celsius_felt_temperature = int(data_to_json['main']['feels_like'] - 273)
+        pressure_level = data_to_json['main']['pressure']
+        humidity_level = data_to_json['main']['humidity']
+        wind_speed = data_to_json['wind']['speed']
+        all_information = (("City: " + city), ("Country: " + country), ("Weather Conditions: " + conditions), ("Temperature: " + str(celsius_temperature)), ("Felt temperature: " + str(celsius_felt_temperature)), ("Pressure: " + str(pressure_level)), ("Humidity: " + str(humidity_level)), ("Wind speed: " + str(wind_speed)))
+        return all_information
+    else:
+        return None
+
+def search():
+    city = search_city.get()
+    weather = get_weather(city)
+    if weather:
+        city_label['text'] = '{}'.format(weather[0])
+        country_label['text'] = '{}'.format( weather[1])
+        weather_conditions_label['text'] = '{}'.format(weather[2])
+        temperature_label['text'] = '{} °C'.format(weather[3])
+        felt_temperature_label['text'] = '{} °C'.format(weather[4])
+        pressure_label['text'] = '{} hPa'.format(weather[5])
+        humidity_label['text'] = '{}%'.format(weather[6])
+        wind_speed_label['text'] = '{}m/s'.format(weather[7])
+    else:
+        return "Error"
